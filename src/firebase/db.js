1fd1971dfee20e8291c71faadbcaa064fe8550b7
Drishtant Regmi <drishtantregmi777@gmail.com>
2021-03-09 22:48:06 +0545

    API route change

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -1,16 +1,27 @@
 import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 
+const {
+  VUE_APP_DB_API_KEY,
+  VUE_APP_AUTH_DOMAIN,
+  VUE_APP_DB_URL,
+  VUE_APP_PROJECT,
+  VUE_APP_STORAGE,
+  VUE_APP_MESSAGING_SENDER,
+  VUE_APP_APP_ID,
+  VUE_APP_SENDER_ID,
+} = process.env;
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAULFPRX3zcuKrL7yZe16CmlKoxQturaKQ",
-    authDomain: "chat-app-5b9cd.firebaseapp.com",
-    databaseURL: "https://chat-app-5b9cd.firebaseio.com",
-    projectId: "chat-app-5b9cd",
-    storageBucket: "chat-app-5b9cd.appspot.com",
-    messagingSenderId: "111623630453",
-    appId: "1:111623630453:web:cb358a941906f8007c523c",
-    measurementId: "G-GG6Z4ZHX8F"
+  apiKey: VUE_APP_DB_API_KEY,
+  authDomain: VUE_APP_AUTH_DOMAIN,
+  databaseURL: VUE_APP_DB_URL,
+  projectId: VUE_APP_PROJECT,
+  storageBucket: VUE_APP_STORAGE,
+  messagingSenderId: VUE_APP_MESSAGING_SENDER,
+  appId: VUE_APP_APP_ID,
+  measurementId: VUE_APP_SENDER_ID,
 };
 
 // Initialize Firebase
